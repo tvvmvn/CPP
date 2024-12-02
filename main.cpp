4697516bc602c8c64a6b051933fc9929b69cfa2e
tvvmvn <tvvmvn@gmail.com>
2024-12-02 20:26:12 +0900

    12.2

--- main.cpp
+++ main.cpp
@@ -2,20 +2,4 @@
 #include <string>
 using namespace std;
 
-class Test {
-  public:
-    void f() {
-      cout << "f invoked";
-    }
-};
-
-int main() {
-  // object creation with new keyword
-  Test* t = new Test();
-
-  t->f();
-
-  // you should delete object created with new keyword.
-  // (The compiler provides a default destructor).
-  delete t;
-}
+int main() {};
